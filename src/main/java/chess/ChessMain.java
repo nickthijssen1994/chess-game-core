d1b75f1fb422363082cbbea7bec736763be986da
Nick Thijssen <nickthijssen1994@gmail.com>
2020-10-11 18:33:05 +0200

    Added Computer VS Computer Game

--- src/main/java/chess/ChessMain.java
+++ src/main/java/chess/ChessMain.java
@@ -1,15 +1,11 @@
 package chess;
 
-import chess.game.ChessGame;
+import chess.game.ComputerGame;
 import chess.game.Game;
-import chess.game.player.PlayerColor;
 
 public class ChessMain {
 
     public static void main(String[] args) {
-        Game game = new ChessGame(true);
-        game.addPlayer(PlayerColor.WHITE, "Nick");
-        game.setPlayerReady(PlayerColor.WHITE);
-        game.makeMove(PlayerColor.WHITE, 0, 1, 0, 2);
+        Game game = new ComputerGame(true);
     }
 }
