52693f4135b28d4a96f156d0bf0a2fe2cb5033f8
Nick Thijssen <nickthijssen1994@gmail.com>
2020-10-15 15:14:12 +0200

    Update Bishop.java

--- src/main/java/chess/engine/pieces/Bishop.java
+++ src/main/java/chess/engine/pieces/Bishop.java
@@ -1,5 +1,46 @@
 package chess.engine.pieces;
 
-public class Bishop {
+import chess.engine.PlayerColor;
+import chess.engine.board.*;
 
+import java.util.*;
+
+public class Bishop extends Piece {
+
+	private static final int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9, -7, 7, 9};
+
+	Bishop(final int piecePosition, final PlayerColor playerColor) {
+		super(piecePosition, playerColor);
+	}
+
+	@Override
+	public List<Move> calculateLegalMoves(final Board board) {
+
+		final List<Move> legalMoves = new ArrayList<>();
+
+		for (final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES) {
+			int candidateDestinationCoordinate = this.piecePosition;
+			while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+				candidateDestinationCoordinate += candidateCoordinateOffset;
+
+				if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+					final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+					if (!candidateDestinationTile.isTileOccupied()) {
+						legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+					} else {
+						final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+						final PlayerColor pieceColor = pieceAtDestination.getPlayerColor();
+
+						if (this.playerColor != pieceColor) {
+							legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate,
+									pieceAtDestination));
+						}
+					}
+					break;
+				}
+			}
+		}
+
+		return Collections.unmodifiableList(legalMoves);
+	}
 }
